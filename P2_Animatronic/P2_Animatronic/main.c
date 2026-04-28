1895389848ec7710522c64ece9049d0fbbc50b7b
esc23586 <esc23586@uvg.edu.gt>
2026-04-28 10:48:29 -0600

    Creación del documento

---
+++ P2_Animatronic/P2_Animatronic/main.c
@@ -0,0 +1,18 @@
+/*
+ * P2_Animatronic.c
+ *
+ * Created: 28/04/2026 09:50:14
+ * Author : grett
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
